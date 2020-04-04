a9f65d64d63d931ef4728a2b1cb19ce157959e1a
Muhammad Elzandi Yusuf <elzandimilanisty29@gmail.com>
2020-04-04 14:03:55 +0700

    update share link

--- src/containers/ProductDetail/ProductDetail.js
+++ src/containers/ProductDetail/ProductDetail.js
@@ -12,6 +12,8 @@ const ProductDetail = (props) => {
     const { prods } = props;
     const paramId = props.match.params.id;
     const [products, setProducts] = useState([]);
+    const shareUrl = 'https://github.com';
+    const [show, setShow] = useState(false);
 
     const filteredProductsHandler = useCallback(filterProducts => {
         const filteredById = filterProducts.filter(data => {
@@ -27,42 +29,48 @@ const ProductDetail = (props) => {
     const backHandler = () => {
         props.history.goBack();
     }
+
+    const shareLinkHandler = () => {
+        setShow(!show)
+    }
     
     return (
         <div>
-            {props.message && 
-                <Modal message="Thank you for buying" />
-            }
             {products.length > 0 &&
-                <div className="product-detail">
-                    <div className="back" onClick={backHandler}>
-                        <i className="fas fa-long-arrow-alt-left"></i>
-                    </div>
-                    <div className="sahre">
-                        <button>
-                            <i className="fas fa-share-alt"></i>
-                        </button>
-                    </div>
-                    <div className="images">
-                        <img src={products[0].imageUrl} alt={products[0].title} />
-                    </div>
-                    <div className="content">
-                        <div className="title">{products[0].title}</div>
-                        <div className="love">
-                            {products[0].loved === 1 ? (
-                                <i className="fas fa-heart"></i>
-                            ):(
-                                <i className="far fa-heart"></i>
-                            )}
+                <div>
+                    {show &&
+                        <Modal close={shareLinkHandler} shareUrl={shareUrl} title={products[0].title} />
+                    }
+                    <div className="product-detail">
+                        <div className="back" onClick={backHandler}>
+                            <i className="fas fa-long-arrow-alt-left"></i>
                         </div>
-                    </div>
-                    <div className="description">
-                        {products[0].description}
-                    </div>
-                    <div className="content">
-                        <div className="price">{products[0].price}</div>
-                        <div className="buy" onClick={() => props.onProductAdded(paramId)}>
-                            <Button icon={null} name="Buy" btnClass="btn btn-buy" />
+                        <div className="sahre">
+                            <button onClick={shareLinkHandler}>
+                                <i className="fas fa-share-alt"></i>
+                            </button>
+                        </div>
+                        <div className="images">
+                            <img src={products[0].imageUrl} alt={products[0].title} />
+                        </div>
+                        <div className="content">
+                            <div className="title">{products[0].title}</div>
+                            <div className="love">
+                                {products[0].loved === 1 ? (
+                                    <i className="fas fa-heart"></i>
+                                ):(
+                                    <i className="far fa-heart"></i>
+                                )}
+                            </div>
+                        </div>
+                        <div className="description">
+                            {products[0].description}
+                        </div>
+                        <div className="content">
+                            <div className="price">{products[0].price}</div>
+                            <div className="buy" onClick={() => props.onProductAdded(paramId)}>
+                                <Button icon={null} name="Buy" btnClass="btn btn-buy" />
+                            </div>
                         </div>
                     </div>
                 </div>
